23017cf800e94b463712543ed1726f19e832367b
Ícaro Harry <icaro.harry@toptal.com>
2024-02-01 18:36:22 -0300

    Fix API_URL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
 });
 
 export default axiosInstance;
